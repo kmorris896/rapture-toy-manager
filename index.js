0d245bc37ae5bfe7c7d462e9279a0e5521f61204
Kelly Morris <70837349+kmorris896@users.noreply.github.com>
2021-09-18 17:19:36 +0000

    Updated to Discord 13

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 require('dotenv').config();
-const winston = require('winston');
-const Discord = require('discord.js');
 
-// Winston Logger Declarations
+// ---------- Winston Logger Declarations
+const winston = require('winston');
+const log_level = process.env.LOG_LEVEL || 'info';
 const logger = winston.createLogger({
-  level: 'info',
+  level: log_level,
   transports: [
     new winston.transports.Console({format: winston.format.combine(winston.format.colorize(), winston.format.simple())}),
     new winston.transports.File({filename: 'logs/combined.log'})
   ]
 });
 
-const TOKEN = process.env.TOKEN;
-const PREFIX = '&';
+// ---------- Discord.js Declarations
+const {Client, Intents, Discord } = require('discord.js');
+const DiscordCollection = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES] });
+client.commands = new DiscordCollection.Collection();
+client.botConfig = {};    // Holds the bot configuration
+client.checkNewArrivalInterval = {};  // Holds the interval for checking for new arrivals
+client.logger = logger;
 
-// Discord.js Declarations
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
 
-// Load Commands
+// ---------- Load Commands
 const botCommands = require('./commands');
 Object.keys(botCommands).map(key => {
-  bot.commands.set(botCommands[key].name, botCommands[key]);
+  client.commands.set(botCommands[key].name, botCommands[key]);
 });
 
 // Start Bot
-bot.login(TOKEN);
+const TOKEN = process.env.DISCORD_TOKEN;
+const PREFIX = process.env.BOT_PREFIX || '&';
 
-bot.on('ready', () => {
-  logger.info(`Logged in as ${bot.user.tag}!`);
+client.once('ready', () => {
+  logger.info(`Logged in as ${client.user.tag}!`);
+  client.logger = logger;
 });
 
+client.login(TOKEN);
+
 // Process Messages
-bot.on('message', msg => {
+client.on('message', msg => {
   // Check to make sure that the message starts with the prefix.  Otherwise, quietly ignore
   if (msg.content.startsWith(PREFIX)) {
     // Remove the prefix and then split the command by arguments.
@@ -40,10 +47,10 @@ bot.on('message', msg => {
     const command = args.shift().toLowerCase();
     logger.info(`Called command: ${command}`);
 
-    if (!bot.commands.has(command)) return;
+    if (!client.commands.has(command)) return;
 
     try {
-      bot.commands.get(command).execute(msg, args);
+      client.commands.get(command).execute(msg, args);
     } catch (error) {
       logger.error(error);
       msg.reply('there was an error trying to execute that command!');
